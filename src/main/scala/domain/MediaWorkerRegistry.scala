fc5327e5f1c6a4cebcdc724129543170679bc3c6
yaroslav <Yaroslav>
2025-02-19 14:09:30 +0300

    [master] Finish MediaWorkerRegistry

--- src/main/scala/domain/MediaWorkerRegistry.scala
+++ src/main/scala/domain/MediaWorkerRegistry.scala
@@ -1,14 +1,16 @@
 package domain
 
-import cats.effect.kernel.{Async, MonadCancel, Resource}
+import cats.effect.kernel.*
 import cats.effect.std.{Queue, Random}
 import cats.implicits.*
+import cats.syntax.all.*
 import cats.{Applicative, Monad}
 
 import scala.collection.mutable
 
 /**
  * A registry that collects all workers in the hospital
+ *
  * @tparam F
  */
 trait MediaWorkerRegistry[F[_]] {
@@ -17,17 +19,21 @@ trait MediaWorkerRegistry[F[_]] {
   def stopMediaWorker(id: MediaWorkerId): F[Either[Throwable, Unit]]
 }
 
+type MediaWorkerSupplier[F[_]] = (MediaWorkerId,
+  MediaWorkerStatus,
+  Int,
+  Queue[F, MediaStream[F]]) => Resource[F, MediaWorker[F]]
+
 class MediaWorkerRegistryImpl[F[_] : Monad : Random : Async]
 (
   storage: mutable.Map[MediaWorkerId, MediaWorker[F]],
-  mediaWorkerSup: (id: MediaWorkerId,
-                   status: MediaWorkerStatus,
-                   tasksCapacity: Int,
-                   queue: Queue[F, MediaStream[F]]) => Resource[F, MediaWorker[F]],
+  mediaWorkerSup: MediaWorkerSupplier[F],
   workerCapacity: Int,
   queue: Queue[F, MediaStream[F]]
 )(implicit val monadCancel: MonadCancel[F, Throwable])
   extends MediaWorkerRegistry[F] {
+  val workersFibers: mutable.Buffer[Fiber[F, Throwable, Any]] = mutable.Buffer.empty[Fiber[F, Throwable, Any]]
+
   override def startNewWorker(): F[MediaWorkerId] =
     for {
       id <- Random[F].nextInt
@@ -37,45 +43,51 @@ class MediaWorkerRegistryImpl[F[_] : Monad : Random : Async]
         workerCapacity,
         queue
       )
-      _ <- Async[F].start(mediaWorkerResource.use { mediaWorker =>
+      fiber <- Async[F].start(mediaWorkerResource.use({ mediaWorker =>
         storage.put(id, mediaWorker)
         Async[F].never
-      })
+      }))
+      _ <- Sync[F].delay {
+        workersFibers.append(fiber)
+      }
     } yield id
 
   override def stopMediaWorker(id: MediaWorkerId): F[Either[Throwable, Unit]] =
     for {
-      optionalMediaWorker <- Applicative[F].pure {
+      optionalMediaWorker <- Sync[F].delay {
         storage.get(id)
       }
       res <- optionalMediaWorker match {
-        case Some(mediaWorker) => mediaWorker.stop.map(Right(_))
+        case Some(mediaWorker) => {
+          mediaWorker.stop.map(Right(_))
+        }
         case None => Applicative[F].pure {
           Left(new Exception("No such media worker"))
         }
       }
     } yield res
 
-  def stopAllWorkers: F[Unit] = {
+  def stopAllWorkers(): F[Unit] =
     storage.values.toList.traverse_ { mediaWorker => stopMediaWorker(mediaWorker.id) }
-  }
 }
 
 object MediaWorkerRegistryImpl {
   def apply[F[_] : Monad : Random : Async](
                                             storage: mutable.Map[MediaWorkerId, MediaWorker[F]],
-                                            mediaWorkerSup: (id: MediaWorkerId,
-                                                             status: MediaWorkerStatus,
-                                                             tasksCapacity: Int,
-                                                             queue: Queue[F, MediaStream[F]]) => Resource[F, MediaWorker[F]],
+                                            mediaWorkerSup: MediaWorkerSupplier[F],
                                             workerCapacity: Int,
                                             queue: Queue[F, MediaStream[F]]
-                                          ): Resource[F, MediaWorkerRegistry[F]] = {
+                                          )(implicit monadCancel: MonadCancel[F, Throwable]): Resource[F, MediaWorkerRegistry[F]] = {
     Resource.make(Applicative[F].pure(new MediaWorkerRegistryImpl(
       storage,
       mediaWorkerSup,
       workerCapacity,
       queue
-    ))) { registry => registry.stopAllWorkers }
+    )))(registry => monadCancel.uncancelable { poll =>
+      registry.stopAllWorkers() *>
+        registry.workersFibers.toList.traverse_ {
+          _.cancel
+        }
+    })
   }
 }
--- src/test/scala/domain/MediaWorkerRegistrySpec.scala
+++ src/test/scala/domain/MediaWorkerRegistrySpec.scala
@@ -12,15 +12,11 @@ import scala.concurrent.duration.DurationInt
 
 class MediaWorkerRegistrySpec extends flatspec.AnyFlatSpec with MockitoSugar {
 
-  // Mocks for dependencies
   val mockQueue: Queue[IO, MediaStream[IO]] = mock[Queue[IO, MediaStream[IO]]]
   implicit val mockRandom: Random[IO] = mock[Random[IO]]
   val mockMediaWorkerSup: (MediaWorkerId, MediaWorkerStatus, Int, Queue[IO, MediaStream[IO]]) => Resource[IO, MediaWorker[IO]] = mock[(MediaWorkerId, MediaWorkerStatus, Int, Queue[IO, MediaStream[IO]]) => Resource[IO, MediaWorker[IO]]]
-  val mockStorage = mutable.Map.empty[MediaWorkerId, MediaWorker[IO]]
 
-  // Test for starting a new worker
   "startNewWorker" should "generate a new MediaWorkerId and add the worker to storage" in {
-    // Arrange
     val mockId = 123
     when(mockRandom.nextInt).thenReturn(IO.pure(mockId))
     var stoppedWorkers = 0
@@ -36,12 +32,12 @@ class MediaWorkerRegistrySpec extends flatspec.AnyFlatSpec with MockitoSugar {
 
     val registryResource: Resource[IO, MediaWorkerRegistry[IO]] = MediaWorkerRegistryImpl[IO](
       storage,
-      (id, _, _, _) => Resource.eval { ??? },
+      (id, _, _, _) => Resource.eval(IO(makeWorker(id))),
       workerCapacity = 10,
       queue = mockQueue
     )
 
-    registryResource.use { registry =>
+    registryResource.use({ registry =>
       for {
         workerId <- registry.startNewWorker()
         _ <- IO.sleep(1.second)
@@ -50,82 +46,55 @@ class MediaWorkerRegistrySpec extends flatspec.AnyFlatSpec with MockitoSugar {
           assert(storage.contains(mockId))
         }
       } yield ()
-    }.unsafeRunSync()
+    }).unsafeRunSync()
 
-    assert(stoppedWorkers == 2)
+    assert(stoppedWorkers == 1)
   }
 
-  // Test for stopping a media worker
   "stopMediaWorker" should "return Right if the worker exists" in {
-    // Arrange
     val mockId = 123
     val mockWorker = mock[MediaWorker[IO]]
-    when(mockStorage.get(mockId)).thenReturn(Some(mockWorker))
-    when(mockWorker.stop).thenReturn(IO.unit)
+    val storage = mutable.Map(mockId -> mockWorker)
+    var stopped = 0
+    when(mockWorker.stop).thenReturn(IO.delay { stopped += + 1} *> IO.unit)
 
-    val registry = new MediaWorkerRegistryImpl[IO](
-      mockStorage,
-      mockMediaWorkerSup,
-      workerCapacity = 10,
-      queue = mockQueue
-    )
-
-    // Act
-    val result = registry.stopMediaWorker(mockId)
-
-    // Assert
-    result.map {
-      case Right(_) => succeed  // Ensure Right is returned for successful stop
-      case Left(_) => fail("Expected Right, got Left")
-    }
-  }
-
-  it should "return Left if the worker does not exist" in {
-    // Arrange
-    val mockId = 123
-    when(mockStorage.get(mockId)).thenReturn(None)
-
-    val registry = new MediaWorkerRegistryImpl[IO](
-      mockStorage,
-      mockMediaWorkerSup,
-      workerCapacity = 10,
-      queue = mockQueue
-    )
-
-    // Act
-    val result = registry.stopMediaWorker(mockId)
-
-    // Assert
-    result.map {
-      case Left(e) => assert(e.getMessage == "No such media worker")  // Ensure Left with the correct message
-      case Right(_) => fail("Expected Left, got Right")
-    }
-  }
-
-  // Test for stopping all workers
-  "stopAllWorkers" should "stop all workers in the storage" in {
-    // Arrange
-    val mockId1: Int = 123
-    val mockId2: Int = 456
-    val mockWorker1 = mock[MediaWorker[IO]]
-    val mockWorker2 = mock[MediaWorker[IO]]
-    val storage = mutable.Map.empty[MediaWorkerId, MediaWorker[IO]] + (mockId1 -> mockWorker1, mockId2 -> mockWorker2)
-    var stoppedWorkers = 0
-    when(mockWorker1.stop).thenReturn(IO.delay { stoppedWorkers = stoppedWorkers + 1 } *> IO.unit)
-    when(mockWorker2.stop).thenReturn(IO.delay { stoppedWorkers = stoppedWorkers + 1 } *> IO.unit)
-
-    val registry = new MediaWorkerRegistryImpl[IO](
+    val registryResource = MediaWorkerRegistryImpl[IO](
       storage,
       mockMediaWorkerSup,
       workerCapacity = 10,
       queue = mockQueue
     )
 
-    // Act
-    val result = registry.stopAllWorkers
+    registryResource.use { registry =>
+      for {
+        result <- registry.stopMediaWorker(mockId)
+      } yield result match {
+        case Right(_) => succeed  // Ensure Right is returned for successful stop
+        case Left(_) => fail("Expected Right, got Left")
+      }
+    }.unsafeRunSync()
 
-    // Assert
-    result.map(_ => succeed) // Ensure no errors are thrown
-    assert(stoppedWorkers == 2)
+    assert(stopped == 1)
+  }
+
+  it should "return Left if the worker does not exist" in {
+    val mockId = 123
+    val storage = mutable.Map.empty[MediaWorkerId, MediaWorker[IO]]
+
+    val registryResource = MediaWorkerRegistryImpl[IO](
+      storage,
+      mockMediaWorkerSup,
+      workerCapacity = 10,
+      queue = mockQueue
+    )
+
+    registryResource.use { registry =>
+      for {
+        result <- registry.stopMediaWorker(mockId)
+      } yield result match {
+        case Left(e) => assert(e.getMessage == "No such media worker")  // Ensure Left with the correct message
+        case Right(_) => fail("Expected Left, got Right")
+      }
+    }.unsafeRunSync()
   }
 }
