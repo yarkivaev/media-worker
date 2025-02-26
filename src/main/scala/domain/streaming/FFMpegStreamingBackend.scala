06a3b4de5a0f4ffcbfaa305204e13b680b0a9893
yaroslav <Yaroslav>
2025-02-27 01:14:04 +0300

    [ffmpeg] Introduce tests for RunProcess

--- src/main/scala/domain/streaming/FFMpegStreamingBackend.scala
+++ src/main/scala/domain/streaming/FFMpegStreamingBackend.scala
@@ -1,11 +1,12 @@
 package domain.streaming
 
+import cats.Functor
 import cats.effect.Sync
 import domain.{MediaSink, MediaSource}
 
 import scala.sys.process.*
 
-class FFMpegStreamingBackend[F[_] : Sync]
+class FFMpegStreamingBackend[F[_] : Sync : RunProcess : Functor]
 (
   using sourceStreamingResource: StreamingResource[MediaSource],
   sinkStreamingResource: StreamingResource[MediaSink]
@@ -17,11 +18,11 @@ class FFMpegStreamingBackend[F[_] : Sync]
         ++ Seq("-i", sourceStreamingResource.destination(mediaSource))
         ++ sinkStreamingResource.options(mediaSink).toList.flatMap((key, value) => List(key, value))
         ++ Seq(sinkStreamingResource.destination(mediaSink))
-    Sync[F].delay({
-      val process = command.run(ProcessLogger(line => {
+    RunProcess[F]().run(
+      command,
+      ProcessLogger(line => {
         //      println(line)
-      }))
-      process.exitValue()
-    })
+      })
+    ).use_
   }
 }
