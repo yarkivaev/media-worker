addc4f006bde49cb02620791ded05b8a7ff47748
yaroslav <Yaroslav>
2025-02-27 16:47:20 +0300

    [ffmpeg] introduce Storage

--- src/main/scala/domain/persistence/Storage.scala
+++ src/main/scala/domain/persistence/Storage.scala
@@ -1,11 +1,13 @@
 package domain.persistence
 
-import cats.effect.IO
+import cats.effect.{Concurrent, IO}
 
-trait Storage[F[_], A] {
+trait Storage[F[_]: Concurrent, A] {
   def save(obj: A): F[Unit]
 }
 
 object Storage {
-  def fake[A]: Storage[IO, A] = (obj: A) => IO.pure(())
+  def fake[A]: Storage[IO, A] = new Storage[IO, A] {
+    override def save(obj: A): IO[Unit] = IO.pure(())
+  }
 }
